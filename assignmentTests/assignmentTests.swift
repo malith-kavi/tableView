477942b8718077878df82dba9161b600d6ecb0eb
Malith Kavinda <138204165+malith-kavi@users.noreply.github.com>
2024-10-11 22:34:12 +0530

    Initial Commit

---
+++ assignmentTests/assignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  assignmentTests.swift
+//  assignmentTests
+//
+//  Created by Malith Kavinda on 2024-10-11.
+//
+
+import Testing
+@testable import assignment
+
+struct assignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
